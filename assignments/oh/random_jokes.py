55b06494b170013d2fdfc37b54ec6d3ca4a1754f
Jason Oh <jasonoh1998@gmail.com>
2023-02-22 14:59:54 -0500

    oh-added an executable file

---
+++ assignments/oh/random_jokes.py
@@ -0,0 +1,28 @@
+# this is a python file that generates the joke
+# the jokes are generated from chatgpt
+import random
+
+jokes = [
+    "Why did the scarecrow win an award? Because he was outstanding in his field!",
+    "What do you call a fake noodle? An impasta!",
+    "Why don't scientists trust atoms? Because they make up everything!", 
+    "Why don't oysters give to charity? Because they're shellfish!",    
+    "Why did the tomato turn red? Because it saw the salad dressing!",    
+    "Why did the coffee file a police report? It got mugged!",    
+    "Why did the chicken cross the playground? To get to the other slide!",   
+    "Why did the math book look so sad? Because it had too many problems!",   
+    "Why do we tell actors to 'break a leg?' Because every play has a cast!",    
+    "Why did the banana go to the doctor? Because it wasn't peeling well!",    
+    "Why did the hipster burn his tongue? He drank his coffee before it was cool.",    
+    "Why was the computer cold? It left its Windows open.",    
+    "Why did the bicycle fall over? Because it was two-tired.",    
+    "Why did the golfer wear two pairs of pants? In case he got a hole in one.",    
+    "What do you call a snowman with a six-pack? An abdominal snowman!",    
+    "Why don't scientists trust atoms? Because they make up everything!",    
+    "Why don't ants get sick? Because they have tiny ant-bodies!",    
+    "What do you call a fake noodle? An impasta!",    
+    "What do you call an alligator in a vest? An investigator!",   
+    "Why did the tomato turn red? Because it saw the salad dressing!"
+]
+
+print(random.choice(jokes))
