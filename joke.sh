c31fb2d6d8bee238941a010119d093050a384b1f
Krish Sapru <67235656+ksapru@users.noreply.github.com>
2023-03-22 15:44:11 -0400

    Delete joke.sh

--- joke.sh
+++
@@ -1,9 +0,0 @@
-
-# This script downloads and prints a joke from the public site "https://icanhazdadjoke.com"
-# To run this file, type ./joke.sh from this folder. 
-
-JOKE=$(curl -sS https://icanhazdadjoke.com | tail -n 1)
-
-echo "$JOKE"
-echo
-exit 0
